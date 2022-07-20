ce5935232f9846d17a156ffdbcc67638168183f0
Paballo27 <Paballo27@bitbucket.org>
2022-07-20 22:03:46 +0200

    list

--- src/Components/home.js
+++ src/Components/home.js
@@ -6,9 +6,12 @@ import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Skeleton from '@mui/material/Skeleton';
-import { ListItem } from '@mui/material';
 import Divider from '@mui/material/Divider';
-import List from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+
 
 const data = [
   {
@@ -84,11 +87,31 @@ export default function SimpleContainer() {
   return (
     <React.Fragment container>
       <CssBaseline />
-      <List>
-      <ListItem>
-        <Typography>Test</Typography>
-      </ListItem>
-      </List>
+      <List
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+        position: 'relative',
+        overflow: 'auto',
+        maxHeight: 300,
+        '& ul': { padding: 0 },
+      }}
+      subheader={<li />}
+    >
+      {[0, 1, 2, 3, 4].map((sectionId) => (
+        <li key={`section-${sectionId}`}>
+          <ul>
+            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+            {[0, 1, 2].map((item) => (
+              <ListItem key={`item-${sectionId}-${item}`}>
+                <ListItemText primary={`Item ${item}`} />
+              </ListItem>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </List>
       <Divider orientation="vertical" flexItem>
       </Divider>
        <Grid 
